81384b81c35e2dcca1b170b60415b75fba1449a3
Mostafa Habib <habib.mostafa@gmail.com>
2022-12-14 11:56:46 -0500

    code for official scrimmage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpConfig.java
@@ -4,9 +4,9 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class TeleOpConfig {
     public static double CLAW_LEFT_CLOSED = 0.55;
-    public static double CLAW_LEFT_OPEN = 0.9;
+    public static double CLAW_LEFT_OPEN = 0.73;
     public static double CLAW_RIGHT_CLOSED = 0.32;
-    public static double CLAW_RIGHT_OPEN = 0.0;
-    public static double SLIDE_SPEED = 1.0;
+    public static double CLAW_RIGHT_OPEN = 0.15;
+    public static double SLIDE_SPEED = 0.9;
     public static double SPIN_SPEED = -0.4;
 }
