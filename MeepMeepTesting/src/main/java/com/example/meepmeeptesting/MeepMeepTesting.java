d9c6a2d1efd7059c536521ddae9f0363d2585cc5
Arshad Anas <arshadanas900+github@gmail.com>
2022-12-30 21:30:35 -0500

    meepmeep testing

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -8,18 +8,17 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(700);
+        MeepMeep meepMeep = new MeepMeep(900);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(65, 65, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(35.3, -60.2, Math.toRadians(-88.8)))
-                                .forward(-47)
-                                .splineTo(new Vector2d(47,11), Math.toRadians(0))
-                                .splineTo(new Vector2d(58,58), Math.toRadians(0))
-
-
+                        drive.trajectorySequenceBuilder(new Pose2d(35, -60, Math.toRadians(90)))
+                                .splineTo(new Vector2d(35, -35), Math.toRadians(90))
+                                .splineTo(new Vector2d(28, -7), Math.toRadians(120))
+                                .waitSeconds(0.1)
+//                                .splineToConstantHeading(new Vector2d(36, -16), Math.toRadians(-30))
                                 .build()
                 );
 
