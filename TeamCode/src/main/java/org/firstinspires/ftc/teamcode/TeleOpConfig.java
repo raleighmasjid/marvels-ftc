4a4d09d291ebcb43134dbd7d3cf82697986c75c3
Arshad Anas <79271473+Arshad-Anas@users.noreply.github.com>
2023-01-08 07:22:59 -0500

    v1 gripper servo angles fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpConfig.java
@@ -3,10 +3,10 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class TeleOpConfig {
-    public static double CLAW_LEFT_CLOSED = 0.55;
-    public static double CLAW_LEFT_OPEN = 0.70;
-    public static double CLAW_RIGHT_CLOSED = 0.32;
-    public static double CLAW_RIGHT_OPEN = 0.15;
+    public static double CLAW_LEFT_CLOSED = 0.5;
+    public static double CLAW_LEFT_OPEN = 0.74;
+    public static double CLAW_RIGHT_CLOSED = 0.4;
+    public static double CLAW_RIGHT_OPEN = 0.165;
     // public static double SLIDE_SPEED = 0.9;
     // public static double SPIN_SPEED = -0.4;
 }
